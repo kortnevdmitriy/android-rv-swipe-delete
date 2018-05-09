3bc8515a29fff6a4f1c5b6e01fd7bfb78c495ca0
Kortnev Dmitriy <kortnevdmitriy@outlook.com>
2018-05-09 17:06:36 +0300

    Formatting changes

--- app/src/main/java/pl/kitek/rvswipetodelete/MainActivity.kt
+++ app/src/main/java/pl/kitek/rvswipetodelete/MainActivity.kt
@@ -10,33 +10,33 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-
-    private val simpleAdapter = SimpleAdapter((1..5).map { "Item: $it" }.toMutableList())
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = simpleAdapter
-
-        val swipeHandler = object : SwipeToDeleteCallback(this) {
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                val adapter = recyclerView.adapter as SimpleAdapter
-                adapter.removeAt(viewHolder.adapterPosition)
-            }
-        }
-        val itemTouchHelper = ItemTouchHelper(swipeHandler)
-        itemTouchHelper.attachToRecyclerView(recyclerView)
-
-        addItemBtn.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.addItemBtn -> simpleAdapter.addItem("New item")
-        }
-    }
-
+	
+	private val simpleAdapter = SimpleAdapter((1..5).map { "Item: $it" }.toMutableList())
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+		
+		recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+		recyclerView.layoutManager = LinearLayoutManager(this)
+		recyclerView.adapter = simpleAdapter
+		
+		val swipeHandler = object : SwipeToDeleteCallback(this) {
+			override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+				val adapter = recyclerView.adapter as SimpleAdapter
+				adapter.removeAt(viewHolder.adapterPosition)
+			}
+		}
+		val itemTouchHelper = ItemTouchHelper(swipeHandler)
+		itemTouchHelper.attachToRecyclerView(recyclerView)
+		
+		addItemBtn.setOnClickListener(this)
+	}
+	
+	override fun onClick(v: View?) {
+		when (v?.id) {
+			R.id.addItemBtn -> simpleAdapter.addItem("New item")
+		}
+	}
+	
 }
